b554fe2dec9b3c3e1e98660a71cbf70287fe3ef1
Muhammad Adnan Rizki Issanjaya <73287823+MAdnan220@users.noreply.github.com>
2021-05-03 21:10:23 +0700

    Add files via upload

---
+++ cek_login.php
@@ -0,0 +1,50 @@
+<?php 
+// mengaktifkan session pada php
+session_start();
+
+// menghubungkan php dengan koneksi database
+include 'koneksi.php';
+
+// menangkap data yang dikirim dari form login
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+// menyeleksi data user dengan username dan password yang sesuai
+$login = mysqli_query($koneksi,"select * from user where username='$username' and password='$password'");
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+
+// cek apakah username dan password di temukan pada database
+if($cek > 0){
+
+	$data = mysqli_fetch_assoc($login);
+
+	
+	if($data['level']=="admin"){
+
+	
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "admin";
+
+		header("location:admin/dashboard.php");
+
+
+	}else if($data['level']==""){
+		
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "";
+	
+		header("location:halaman_admin.php");
+
+	
+	}else{
+
+		// alihkan ke halaman login kembali
+		header("location:index.php?pesan=gagal");
+	}	
+}else{
+	header("location:index.php?pesan=gagal");
+}
+
+?>
---
+++ halaman_admin.php
@@ -0,0 +1,1 @@
+<h1>selamat datang konsumen/h1>
